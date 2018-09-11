9d0adc9057092a3a5d420d91e6c4e5b4554529fd
Igor Tverdokhleb <itvrd2@yandex.ru>
2018-09-11 20:13:11 +0200

    adminOnly middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'adminOnly' => \App\Http\Middleware\adminOnly::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
